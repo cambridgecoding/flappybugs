4af2941284a1e44cecd586b31a17d2525204db41
raoulDoc <raoul@urma.com>
2015-12-05 22:16:11 +0200

    introducing html bugs

--- bug4/flappy.js
+++ bug4/flappy.js
@@ -1,5 +1,4 @@
-
-var actions = { preload: preload, create: create, update: update };
+var actions = {preload: preload, create: create, update: update};
 
 var game = new Phaser.Game(790, 400, Phaser.AUTO, "game", actions);
 
@@ -15,25 +14,25 @@ var pipeInterval = 1.75;
 
 
 function preload() {
-    
-    game.load.image("playerImg","../assets/jamesBond.gif");
-    
+
+    game.load.image("playerImg", "../assets/jamesBond.gif");
+
     game.load.audio("score", "../assets/point.ogg");
-    
-    game.load.image("pipe","../assets/pipe.png");
+
+    game.load.image("pipe", "../assets/pipe.png");
 }
 
 
 function create() {
-    
+
     game.stage.setBackgroundColor("#F3D3A3");
-    
+
     game.add.text(20, 20, "Welcome to my game",
         {font: "30px Arial", fill: "#FFFFFF"});
-    
+
     labelScore = game.add.text(20, 60, "0",
         {font: "30px Arial", fill: "#FFFFFF"});
-    
+
     player = game.add.sprite(80, 200, "playerImg");
 
 
@@ -44,64 +43,63 @@ function create() {
     player.body.gravity.y = 200;
 
     game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump());
-
 }
 
 
 function update() {
-    
+
 
     game.physics.arcade
         .overlap(player,
-            pipes,
-            gameOver);
+        pipes,
+        gameOver);
 
 }
 
 
 function addPipeBlock(x, y) {
-    
-    var block = game.add.sprite(x,y,"pipe");
-    
+
+    var block = game.add.sprite(x, y, "pipe");
+
     pipes.push(block);
-    
+
     game.physics.arcade.enable(block);
-    
-    
+
+
     block.body.velocity.x = -200;
 }
 
 
 function generatePipe() {
-    
-    
+
+
     var gapStart = game.rnd.integerInRange(1, 5);
-    
+
     for (var count = 0; count < 8; count++) {
-        
-        
-        if(count != gapStart && count != gapStart+1){
+
+
+        if (count != gapStart && count != gapStart + 1) {
             addPipeBlock(750, count * 50);
         }
     }
-    
+
     changeScore();
 }
 
 function playerJump() {
-    
+
     player.body.velocity.y = -200;
 }
 
 
 function changeScore() {
-    
+
     score++;
-    
+
     labelScore.setText(score.toString());
 }
 
 function gameOver() {
-    
+
     game.destroy();
 }
